66d263041037ed6ec8b5160df18a0716ef028045
Pranav Maheshwari <pranav.maheshwari@xneelo.com>
2022-11-22 14:31:49 +0530

    Fixed link off

--- exchangegram_frontend/src/components/Pin.jsx
+++ exchangegram_frontend/src/components/Pin.jsx
@@ -108,18 +108,15 @@ const Pin = ({ pin }) => {
               )}
             </div>
             <div className=" flex justify-between items-center gap-2 w-full">
-              {destination?.slice(8).length > 0 ? (
-                <a
-                  href={destination}
-                  target="_blank"
+              {_id && (
+                <button
                   className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
-                  rel="noreferrer"
                 >
                   {' '}
                   <BsFillArrowUpRightCircleFill />
-                  {destination?.slice(8, 17)}...
-                </a>
-              ) : undefined}
+                  {destination?.slice(0, 5)}...
+                </button>
+              )}
               {postedBy?._id === user?.googleId && (
                 <button
                   type="button"
